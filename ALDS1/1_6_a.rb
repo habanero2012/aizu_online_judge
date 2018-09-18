eedb486c99424516c132391d24e779f5a9576d6f
hsato <sato1101@live.jp>
2018-09-18 23:53:40 +0900

    add 1_6_a

---
+++ ALDS1/1_6_a.rb
@@ -0,0 +1,21 @@
+def counting_sort(arr)
+  result = Array.new(arr.size)
+  counter = Hash.new { |h, k| h[k] = 0 }
+
+  arr.each { |v| counter[v] += 1 }
+
+  1.upto(arr.max) do |v|
+    counter[v] = counter[v] + counter[v - 1]
+  end
+
+  arr.each do |v|
+    result[counter[v] - 1] = v
+    counter[v] -= 1
+  end
+
+  result
+end
+
+gets
+arr = gets.split.map(&:to_i)
+puts counting_sort(arr).join("\s")
